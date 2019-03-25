60ac1130cbd1c24b25b8f54cc7922aa113790300
omnipede <omnipede@naver.com>
2019-03-25 14:55:13 +0000

    Add graph view.

--- myRoomTempServer/index.js
+++ myRoomTempServer/index.js
@@ -25,12 +25,44 @@ app.get('/dump', function(req, res) {
 		if(err)
 			throw err;
 		rows = rows.reverse()
+		
 		rows.forEach(function(row) {	
 			var d = moment(new Date(row.time)).tz('Asia/Seoul').format('YYYYMMDD, HH:mm');
 			var t = d + ', temperature = ' + String(row.value.toFixed(2)) + '\n';
 			res.write(t);
 		});
 		res.end();
+	});
+});
+
+app.get('/graph', function(req, res) {
+
+	var count = req.query.count || 120;
+	count = Number(count);
+	var html = fs.readFile('./test.html', function(err, html) {
+		html = " " + html;
+		
+		connection.query(`select * from sensors order by time DESC limit ${count}`,
+		 function(err, rows, cols) {
+			if (err) throw err;
+			rows = rows.reverse();
+			var data = "";
+			var comma = "";
+			for (var i = 0; i< rows.length; i++) {
+				r = rows[i];
+				data += comma + `[new Date(${Date.parse(r.time)}),${r.value.toFixed(2)}]`;
+				comma = ",";
+			}
+			var header = "data.addColumn('date', 'Date');"
+			header += "data.addColumn('number', 'Temp');"
+
+			html = html.replace("<%HEADER%>", header);
+			html = html.replace("<%DATA%>", data);
+
+			res.writeHeader(200, {"Content-Type": "text/html"});
+			res.write(html);
+			res.end();
+		});
 	});
 });
 
