dd5d7179c0b379d4a0b41a6661b070b5ee5dcf01
omnipede <omnipede@naver.com>
2019-03-28 15:18:28 +0000

    Remove bootstrap cdn.

--- myRoomTempServer/index.js
+++ myRoomTempServer/index.js
@@ -6,6 +6,10 @@ const app = express()
 
 connection.connect();
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.get('/', function(req, res) {
     res.send('@20161596');
 });
