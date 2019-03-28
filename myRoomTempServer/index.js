e3fcfe51645aca8032dc1f893638ca720dbbe0d4
omnipede <omnipede@naver.com>
2019-03-28 14:07:17 +0000

    Add dump area.

--- myRoomTempServer/index.js
+++ myRoomTempServer/index.js
@@ -39,22 +39,27 @@ app.get('/graph', function(req, res) {
 
 	var count = req.query.count || 120;
 	count = Number(count);
-	var html = fs.readFile('./graph.html', function(err, html) {
+	var html = fs.readFile('./front.html', function(err, html) {
 		html = " " + html;	
 		var qry = `select * from sensors order by time DESC limit ${count}`;
 		connection.query(qry, function(err, rows, cols) {
-			if (err) throw err;
+			if (err) 
+				throw err;
 			rows = rows.reverse();
 			var data = "";
 			var comma = "";
+			var dump = "";
 			rows.forEach(function(row) {
 				data += comma + `[new Date(${Date.parse(row.time)}),${row.value.toFixed(2)}]`;
+				dump += moment(new Date(row.time)).tz('Asia/Seoul').format('YYYYMMDD, HH:mm')
+					+ ', temperature = ' + String(row.value.toFixed(2)) + '\n';
 				comma = ",";
 			})
 			var header = "data.addColumn('date', 'Date');"
 			header += "data.addColumn('number', 'Temp');"
 			html = html.replace("<%HEADER%>", header);
 			html = html.replace("<%DATA%>", data);
+			html = html.replace("<%DUMP%>", dump);
 			res.writeHeader(200, {"Content-Type": "text/html"});
 			res.write(html);
 			res.end();
